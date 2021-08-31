e2ba2de8dd8946bff5c4b360d6d0bc6c66ddf70c
tunght91 <hoangtung.utc@gmail.com>
2021-08-31 11:48:16 +0700

    update

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const compression = require('compression');
+const app = express();
+const http = require('http').createServer(app);
+
+app.use(express.json())
+app.use(compression());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept')
+  if (req.url.lastIndexOf('wasm.br') == req.url.length - 7) {
+    res.header('Content-Type', 'application/wasm')
+    res.header('Content-Encoding', 'br')
+  }
+  else if (req.url.lastIndexOf('.br') == req.url.length - 3) {
+    res.header('Content-Encoding', 'br')
+  }
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
+
+app.use(express.static('public'))
+
+http.listen(process.env.PORT || 8020, async (err) => {
+  console.log('start v2');
+  console.log('server started', process.env.PORT || 8020)
+});
